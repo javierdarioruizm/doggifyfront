cc9beb2de9dc8a7f489c1a1433d6914c68f925bb
javierdarioruizm <javierdarioruizm@gmail.com>
2021-03-29 17:56:36 +0200

    Actualización de app.modules con importacion de la libreria Angular Map  y AgmSnazzy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LugaresComponent } from './components/complugares/lugares/lugares.component';
+import { AgmCoreModule } from '@agm/core';
+import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,11 @@ import { LugaresComponent } from './components/complugares/lugares/lugares.compo
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBXoe3vvdGGosbpLVZqUncQDgiW4UAbl58'
+    }),
+    AgmSnazzyInfoWindowModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
