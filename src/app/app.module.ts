dcf4a44f1a8b7c5d338091579175ebe5e446a3c0
javierdarioruizm <javierdarioruizm@gmail.com>
2021-03-29 20:36:57 +0200

    Actuyalización de appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,66 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { HomeComponent } from './components/home/home.component';
+
 import { LugaresComponent } from './components/complugares/lugares/lugares.component';
 import { AgmCoreModule } from '@agm/core';
 import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
+import { MapalugarComponent } from './components/complugares/mapalugar/mapalugar.component';
+
 import { NewlugarComponent } from './components/complugares/newlugar/newlugar.component';
 import { EditlugarComponent } from './components/complugares/editlugar/editlugar.component';
-import { MapalugarComponent } from './components/complugares/mapalugar/mapalugar.component';
 import { VistalugarComponent } from './components/complugares/vistalugar/vistalugar.component';
-import { ValoracionesComponent } from './components/complugares/valoraciones/valoraciones.component';
 import { ValoracionmediaComponent } from './components/complugares/valoracionmedia/valoracionmedia.component';
-import { HeaderuserComponent } from './components/compusuarios/headeruser/headeruser.component';
-import { CuentaComponent } from './components/compusuarios/cuenta/cuenta.component';
+import { ValoracionesComponent } from './components/complugares/valoraciones/valoraciones.component';
+
+
+
 import { NewComponent } from './components/compusuarios/new/new.component';
 import { LoginComponent } from './components/compusuarios/login/login.component';
+import { CuentaComponent } from './components/compusuarios/cuenta/cuenta.component';
 import { PerfilComponent } from './components/compusuarios/perfil/perfil.component';
 import { FavoritosComponent } from './components/compusuarios/favoritos/favoritos.component';
 import { ComentariosComponent } from './components/compusuarios/comentarios/comentarios.component';
+
+import { HeaderuserComponent } from './components/headeruser/headeruser.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LugaresComponent,
     FooterComponent,
     HomeComponent,
+    LugaresComponent,
     NewlugarComponent,
     EditlugarComponent,
     MapalugarComponent,
     VistalugarComponent,
-    ValoracionesComponent,
     ValoracionmediaComponent,
+    ValoracionesComponent,
+
     HeaderuserComponent,
-    CuentaComponent,
-    NewComponent,
     LoginComponent,
+    NewComponent,
     PerfilComponent,
+    CuentaComponent,
     FavoritosComponent,
-    ComentariosComponent
+    ComentariosComponent,
+
   ],
+
+
   imports: [
     BrowserModule,
     AppRoutingModule,
