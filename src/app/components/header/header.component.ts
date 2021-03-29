1fc0abf3464e7fe86092340c6ef78eef787f323e
javierdarioruizm <javierdarioruizm@gmail.com>
2021-03-29 17:37:02 +0200

    Componente creado con menú superior

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  icono: string;
+  enlace: string;
+  estado: boolean;
+  menuCerrado: boolean;
+  menuAbierto: boolean;
+  faTimes = faTimes;
+  faBars = faBars;
+  faUserCircle = faUserCircle;
+  menu: boolean;
 
-  ngOnInit(): void {
+
+  constructor() {
+
+    this.icono = 'unclicked';
+    this.enlace = 'enlaceunclicked';
+    this.menuCerrado = true;
+    this.menuAbierto = false;
+    this.menu = false;
   }
 
+  ngOnInit(): void {
+
+    console.log(this.icono);
+    console.log(this.menu);
+
+  }
+
+
+
+  onClick() {
+
+    if (this.icono === 'unclicked') {
+      this.icono = 'clicked';
+      this.menu = !this.menu
+
+    } else {
+      this.icono = 'unclicked';
+      this.menu = !this.menu
+    }
+    console.log(this.icono);
+    console.log(this.menu);
+  }
+
+  onClickEnlace() {
+    if (this.icono === 'clicked') {
+      this.icono = 'unclicked';
+      this.menu = !this.menu
+
+    } else {
+      this.icono = 'clicked';
+      this.menu = !this.menu
+    }
+    setTimeout(() => window.location.reload(), 20);
+
+  }
+
+
+
+
+
 }
+
