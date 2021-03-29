66f9be575f957fcb1befcc03ec92cdd6d1c3b911
javierdarioruizm <javierdarioruizm@gmail.com>
2021-03-29 18:24:29 +0200

    Instalación de FontAwesome e importación en app.module de HttpModule y ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { LugaresComponent } from './components/complugares/lugares/lugares.component';
 import { AgmCoreModule } from '@agm/core';
 import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -17,10 +21,14 @@ import { AgmSnazzyInfoWindowModule } from '@agm/snazzy-info-window';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBXoe3vvdGGosbpLVZqUncQDgiW4UAbl58'
     }),
     AgmSnazzyInfoWindowModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
